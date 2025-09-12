602f211c54eedbdef638c1a3a1c76e41dff0b083
Saucesamba <arhipartyom@yandex.ru>
2025-09-12 15:07:15 +0300

    add 17-th task solution

--- 1-17/main.go
+++ 1-17/main.go
@@ -6,18 +6,24 @@ import (
 )
 
 func BinarySearch(arr []int, item int) int {
-	if len(arr) <= 1 && arr[0] != item {
+	return binarySearchRecursive(arr, item, 0, len(arr)-1)
+}
+
+func binarySearchRecursive(arr []int, item, low, high int) int {
+	if low > high {
 		return -1
 	}
-	mid := len(arr) / 2
+
+	mid := (low + high) / 2
 	if arr[mid] == item {
 		return mid
 	} else if item < arr[mid] {
-		return BinarySearch(arr[:mid], item)
+		return binarySearchRecursive(arr, item, low, mid-1)
 	} else {
-		return BinarySearch(arr[mid+1:], item)
+		return binarySearchRecursive(arr, item, mid+1, high)
 	}
 }
+
 func main() {
 	arr := []int{1, 52, 10, 0, 4, 33, 6, 3, 134, 1356, 36, 7}
 	sort.Ints(arr)
